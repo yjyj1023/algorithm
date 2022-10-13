49a380c612cdd177e6080769816f181e74cf2572
yjyj1023 <lyj19961023@gmail.com>
2022-10-13 10:06:10 +0900

    Sort 메소드에 매개변수 num 추가

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,27 +1,29 @@
 public class BubbleSort {
-    public int[] Sort(int[] arr){
+    public int[] Sort(int[] arr, int num) {
         int temp;
 
-        for(int i = 0; i<arr.length; i++){
-            if(arr[0]>arr[i]){
-                temp = arr[0];
-                arr[0] = arr[i];
+        for (int i = num+1; i < arr.length; i++) {
+            if (arr[num] > arr[i]) {
+                temp = arr[num];
+                arr[num] = arr[i];
                 arr[i] = temp;
             }
         }
-
         return arr;
     }
 
-
     public static void main(String[] args) {
-        int[] arr = new int[]{7,2,3,9,28,11};
+        int[] arr = new int[]{7, 2, 3, 9, 28, 11};
 
         BubbleSort bubbleSort = new BubbleSort();
-        int[] result = bubbleSort.Sort(arr);
+        int[] result1 = bubbleSort.Sort(arr, 0);
+        int[] result2 = bubbleSort.Sort(result1, 1);
+        int[] result3 = bubbleSort.Sort(result2, 2);
+        int[] result4 = bubbleSort.Sort(result3, 3);
+        int[] result5 = bubbleSort.Sort(result4, 4);
 
-        for(int i:result){
-            System.out.print(i+" ");
+        for (int i : result5) {
+            System.out.print(i + " ");
         }
     }
 }
