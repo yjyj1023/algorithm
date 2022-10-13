3c72a888a6a6a81568a195bccaf0f56a22d65dde
yjyj1023 <lyj19961023@gmail.com>
2022-10-13 10:16:52 +0900

    swap메소드 추가

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,16 +1,19 @@
 public class BubbleSort {
     public int[] Sort(int[] arr) {
-        int temp;
-
         for(int i =0; i<arr.length-1; i++ ){
             for (int j = i+1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                    Swap(arr, i, j);
                 }
             }
         }
+        return arr;
+    }
+
+    public int[] Swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
 
         return arr;
     }
