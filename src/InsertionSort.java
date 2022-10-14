000cd37fe1f03f0d72467d4748057a1cd42f9022
yjyj1023 <lyj19961023@gmail.com>
2022-10-14 09:46:23 +0900

    삽입정렬 맨처음 swap하기

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,7 +1,17 @@
 public class InsertionSort {
     public int[] sort(int[] arr){
         int i = 1;
+
+        if(arr[i]<arr[i-1]){
+            Swap(arr, i, i-1);
+        }
         return new int[]{};
+    }
+
+    public void Swap(int[]arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 
     public static void main(String[] args) {
