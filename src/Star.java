c6658d1bee9533924f12795c0b4f51b34d87fea6
yjyj1023 <lyj19961023@gmail.com>
2022-10-17 09:21:43 +0900

    별찍기 1. 메소드로 분리하고 for문을 한개 더 써서 2. print로 출력하기 3. n을 받으면 n층으로

--- src/Star.java
+++ src/Star.java
@@ -1,8 +1,15 @@
 public class Star {
+    public void starPrint(int num){
+        for(int i = 1; i <= num; i++){
+            for(int j = 0; j <i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        Star star = new Star();
+
+        star.starPrint(5);
     }
 }
