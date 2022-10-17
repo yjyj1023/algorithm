b866541a2a9a82ea854a6f52fa1b56d817003fa7
yjyj1023 <lyj19961023@gmail.com>
2022-10-17 10:13:14 +0900

    별찍기     *    ***   *****  ******* *********

--- src/Pyramid.java
+++ src/Pyramid.java
@@ -14,7 +14,7 @@ public class Pyramid {
                 System.out.print(" ");
             }
 
-            for(int j = 0; j <1+(i-1)*2; j++){
+            for(int j = 0; j <2*i -1; j++){
                 System.out.print(letter);
             }
             System.out.println();
