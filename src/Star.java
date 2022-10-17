e89fe7a28ae7133f2533ba9a88646894fc17470b
yjyj1023 <lyj19961023@gmail.com>
2022-10-17 09:35:22 +0900

    별찍기 도전과제

--- src/Star.java
+++ src/Star.java
@@ -1,14 +1,22 @@
 public class Star {
+    private String letter = "*";
+
+    public Star(){
+
+    }
+    public Star(String letter){
+        this.letter = letter;
+    }
     public void starPrint(int num){
         for(int i = 1; i <= num; i++){
             for(int j = 0; j <i; j++){
-                System.out.print("*");
+                System.out.print(letter);
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        Star star = new Star();
+        Star star = new Star("#");
 
         star.starPrint(5);
     }
