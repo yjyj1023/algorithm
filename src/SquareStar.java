37cb1fd2bef9e252fd68ea5bec8616634a480624
yjyj1023 <lyj19961023@gmail.com>
2022-10-18 09:14:30 +0900

    별찍기-직사각형

--- src/SquareStar.java
+++ src/SquareStar.java
@@ -7,8 +7,18 @@ public class SquareStar {
             System.out.println();
         }
     }
+    public void printRectangle(int x, int y){
+        for(int i = 0; i<x; i++){
+            for(int j = 0; j<y; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
     public static void main(String[] args) {
         SquareStar squareStar = new SquareStar();
         squareStar.printSquare(5);
+        squareStar.printRectangle(2,3);
     }
 }
