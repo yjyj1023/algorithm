46ca664dd469270bc13c4107dc78a3692d2af549
yjyj1023 <lyj19961023@gmail.com>
2022-10-27 09:17:22 +0900

    프로그래머스 완주하지 못한 선수

---
+++ src/완주하지못한선수.java
@@ -0,0 +1,10 @@
+public class 완주하지못한선수 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
