ea5e921c0b2f1e2bfba7de4c7069e989bd5e1d63
yjyj1023 <lyj19961023@gmail.com>
2022-10-27 09:24:39 +0900

    프로그래머스 완주하지 못한 선수 해시맵 생성까지함

--- src/완주하지못한선수.java
+++ src/완주하지못한선수.java
@@ -1,6 +1,13 @@
+import java.util.HashMap;
+import java.util.Hashtable;
+
 public class 완주하지못한선수 {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        HashMap<Integer,String> member = new HashMap<>();
+        for(int i = 0; i< participant.length; i++){
+            member.put(i,participant[i]);
+        }
         return answer;
     }
 
