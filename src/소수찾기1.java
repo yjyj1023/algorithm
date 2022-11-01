5beff173f44a44b8028fb0f386fe6f7ec4cf30d7
yjyj1023 <lyj19961023@gmail.com>
2022-11-01 09:28:03 +0900

    소수찾기-기본틀작성

---
+++ src/소수찾기1.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class 소수찾기1 {
+
+    //실습: 13, 17, 19, 23 이 소수인지 판별하는 알고리즘을 만들어 보고 소수인지 구해 보세요.
+    //n이 들어왔을때 n-1까지 나누고 나머지가 0이 아니면 소수이다.
+
+    boolean isPrime(int num) {
+        boolean answer = true;
+
+        for (int i = 1; i <= num; i++) {
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        소수찾기1 a = new 소수찾기1();
+
+        boolean result = a.isPrime(13);
+        System.out.println(result);
+
+        boolean result2 = a.isPrime(17);
+        System.out.println(result2);
+
+        boolean result3 = a.isPrime(19);
+        System.out.println(result3);
+
+        boolean result4 = a.isPrime(23);
+        System.out.println(result4);
+    }
+}
