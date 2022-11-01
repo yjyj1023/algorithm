eebf831353975714c6d4a350c499eca685dd8e1b
yjyj1023 <lyj19961023@gmail.com>
2022-11-01 09:49:06 +0900

    소수찾기-템플릿 콜백 적용 전

--- src/PrimeDivideUntil.java
+++ src/PrimeDivideUntil.java
@@ -11,23 +11,46 @@ public class PrimeDivideUntil {
                 answer = false;
             }
         }
+        return answer;
+    }
 
+    //n이 들어왔을때 n/2까지 나누고 나머지가 0이 아니면 소수이다.
+    boolean isPrime2(int num) {
+        boolean answer = true;
+
+        for (int i = 2; i <= num/2; i++) {
+            if(num%i == 0){
+                answer = false;
+            }
+        }
+        return answer;
+    }
+
+    //n이 들어왔을때 루트n까지 나누고 나머지가 0이 아니면 소수이다.
+    boolean isPrime3(int num) {
+        boolean answer = true;
+
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if(num%i == 0){
+                answer = false;
+            }
+        }
         return answer;
     }
 
     public static void main(String[] args) {
         PrimeDivideUntil a = new PrimeDivideUntil();
 
-        boolean result = a.isPrime(13);
+        boolean result = a.isPrime2(15);
         System.out.println(result);
 
-        boolean result2 = a.isPrime(17);
+        boolean result2 = a.isPrime2(17);
         System.out.println(result2);
 
-        boolean result3 = a.isPrime(19);
+        boolean result3 = a.isPrime2(19);
         System.out.println(result3);
 
-        boolean result4 = a.isPrime(23);
+        boolean result4 = a.isPrime2(23);
         System.out.println(result4);
     }
 }
