365493ce6167be94b16928a36548c7af6e03e126
yjyj1023 <lyj19961023@gmail.com>
2022-11-02 10:45:20 +0900

    소수찾기-에라토스테네스체 사용 2번째 방법 추가

--- src/소수찾기.java
+++ src/소수찾기.java
@@ -2,34 +2,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class 소수찾기 {
-    public void solution(int n) {
+
+    //remove 사용(속도가 느림)
+    public int solution(int n) {
         int answer = 0;
 
         List<Integer> num = new ArrayList<>();
         // 2 ~ 50채우기
         for (int i = 2; i <= n; i++) num.add(i);
 
-        for (int i = 2; i < (int)Math.sqrt(n); i++) {
+        for (int i = 2; i <= (int)Math.sqrt(n); i++) {
             for (int j = 0; j < num.size(); j++) {
                 if (num.get(j) % i == 0 && num.get(j) > i) num.remove(j);
             }
-
         }
 
-        System.out.println(num);
-        System.out.println(num.size());
+        return num.size();
+    }
+
+    //remove를 사용하지 않음(더 빠르다)
+    public int solution2(int n) {
+        int answer = 0;
+
+        List<Integer> num = new ArrayList<>();
+        List<Boolean> checks = new ArrayList<>();
+
+        // 2 ~ 50채우기
+        for (int i = 2; i <= n; i++) num.add(i);
+        for (int i = 2; i <= n; i++) checks.add(true);
+
+        for (int i = 2; i <= (int)Math.sqrt(n); i++) {
+            for (int j = 0; j < num.size(); j++) {
+                if (num.get(j) % i == 0 && num.get(j) > i) checks.set(j, false);
+            }
+        }
+        int cnt = 0;
+
+        for (boolean i: checks) {
+            if(i==true){
+                cnt++;
+            }
+        }
+        return cnt;
     }
 
     public static void main(String[] args) {
         소수찾기 a = new 소수찾기();
 
-        a.solution(50);
 
-//        int result1 = a.solution(50);
-//        System.out.println(result1);
-//
-//        int result2 = a.solution(5);
-//        System.out.println(result2
-//        );
+        int result1 = a.solution2(50);
+        System.out.println(result1);
+
+        int result2 = a.solution2(5);
+        System.out.println(result2);
     }
 }
