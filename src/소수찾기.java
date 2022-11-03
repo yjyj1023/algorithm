223c610f556ee43b07e8ff25db1094e2d56eba9b
yjyj1023 <lyj19961023@gmail.com>
2022-11-03 10:00:59 +0900

    소수찾기-에라토스테네스체 사용 2번째 방법 추가 수정2

--- src/소수찾기.java
+++ src/소수찾기.java
@@ -24,27 +24,26 @@ public class 소수찾기 {
     public int solution2(int n) {
         int answer = 0;
 
-        List<Integer> num = new ArrayList<>();
-        List<Boolean> checks = new ArrayList<>();
+        boolean[] checks = new boolean[n+1];
 
         // 초기화
-        for (int i = 2; i <= n; i++) num.add(i);
-        for (int i = 2; i <= n; i++) checks.add(true);
+        checks[0] = false;
+        checks[1] = false;
 
+        for (int i = 2; i < n+1; i++) checks[i] = true;
 
         for (int i = 2; i <= (int)Math.sqrt(n); i++) {
-            for (int j = 2+(i-2)*i; j < num.size(); j+=i) {
-                checks.set(j, false);
+            for (int j = 2*i; j < n+1; j+=i) {
+                checks[j] = false;
             }
         }
 
-        int cnt = 0;
         for (boolean i: checks) {
             if(i==true){
-                cnt++;
+                answer++;
             }
         }
-        return cnt;
+        return answer;
     }
 
     public static void main(String[] args) {
