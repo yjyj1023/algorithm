8d4669b034b0fa11efaff9b6204350e7148e7b0e
yjyj1023 <lyj19961023@gmail.com>
2022-11-07 09:31:37 +0900

    step2 나누어 떨어지는지 체크하기

--- src/하샤드수.java
+++ src/하샤드수.java
@@ -3,6 +3,8 @@ import java.util.Map;
 public class 하샤드수 {
     public boolean solution(int x) {
         boolean answer = true;
+        //원래 x 저장
+        int x2 = x;
 
         //1234 = 1*1000 + 2*100 + 3*10 + 4
         int sumOfDigit = 0;
@@ -10,6 +12,9 @@ public class 하샤드수 {
         while(x>0){
             sumOfDigit += x%10;
             x = x/10;
+        }
+        if(x2 % sumOfDigit != 0){
+            answer = false;
         }
 
         return answer;
