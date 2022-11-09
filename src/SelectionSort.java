d3c9d7cc74d553d8be1b02ff19f0708017d56313
yjyj1023 <lyj19961023@gmail.com>
2022-11-09 09:49:21 +0900

    선택정렬 - 오름차순 정렬 완료

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,19 +1,36 @@
 public class SelectionSort {
+    public int[] Swap(int[] arr, int idx1, int idx2){
+        int temp;
+
+        temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+
+        return arr;
+    }
+
+    public int[] Ascending(int[]arr){  //오름차순 정렬
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i];
+            int minIdx = i;
+            for (int j = i; j < arr.length; j++) {
+                if(arr[j]<min){
+                    min = arr[j];
+                    minIdx = j;
+                }
+            }
+            Swap(arr,i,minIdx);
+        }
+        return arr;
+    }
 
     public static void main(String[] args) {
         int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
 
-        //최소값 찾기
-        int min = arr[0];
-        int minIdx = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]<min){
-                min = arr[i];
-                minIdx = i;
-            }
+        SelectionSort s = new SelectionSort();
+        int[] result = s.Ascending(arr);
+        for(int i: result){
+            System.out.println(i);
         }
-
-        System.out.println(min);
-        System.out.println(minIdx);
     }
 }
