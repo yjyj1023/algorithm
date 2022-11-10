41db9ea0e77c813e4b66214514c70a2b27f65ca8
yjyj1023 <lyj19961023@gmail.com>
2022-11-10 09:28:04 +0900

    같은 숫자는 싫어 - 스택 안쓰고(주석추가)

--- src/같은숫자는싫어.java
+++ src/같은숫자는싫어.java
@@ -5,17 +5,25 @@ public class 같은숫자는싫어 {
     public int[] solution(int []arr) {
         int[] answer = {};
 
+        //정답 리스트 선언
         List<Integer> answerList = new ArrayList<>();
+
+        //배열의 첫번째 수 넣기
         answerList.add(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            if(arr[i] != arr[i-1]){
+
+
+        for (int i = 1; i < arr.length; i++) { //배열의 1~맨끝 까지
+            if(arr[i] != arr[i-1]){  //앞 원소와 같지 않으면 리스트에 넣음
                 answerList.add(arr[i]);
             }
         }
+
+        //리스트 -> 배열로
         answer = new int[answerList.size()];
         for (int i = 0; i < answerList.size(); i++) {
             answer[i] = answerList.get(i);
         }
+
         return answer;
     }
 
