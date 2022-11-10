86d3bc37280433515c5e24dbd1aa265b08e55728
yjyj1023 <lyj19961023@gmail.com>
2022-11-10 10:07:12 +0900

    같은 숫자는 싫어 - 스택 쓰고

--- src/같은숫자는싫어.java
+++ src/같은숫자는싫어.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class 같은숫자는싫어 {
-    public int[] solution(int []arr) {
+    public int[] solution(int []arr) {  //스택 안쓰고
         int[] answer = {};
 
         //정답 리스트 선언
@@ -10,7 +11,6 @@ public class 같은숫자는싫어 {
 
         //배열의 첫번째 수 넣기
         answerList.add(arr[0]);
-
 
         for (int i = 1; i < arr.length; i++) { //배열의 1~맨끝 까지
             if(arr[i] != arr[i-1]){  //앞 원소와 같지 않으면 리스트에 넣음
@@ -27,6 +27,29 @@ public class 같은숫자는싫어 {
         return answer;
     }
 
+    public int[] solution2(int []arr) {  //스택 쓰고
+        int[] answer = {};
+
+        Stack<Integer> answerStack = new Stack<>();
+
+        answerStack.push(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if(answerStack.peek() != arr[i]){
+                answerStack.push(arr[i]);
+            }
+        }
+
+        //스택 -> 배열로
+        answer = new int[answerStack.size()];
+        for (int i = answerStack.size()-1; i >= 0 ; i--) { //스택이 거꾸로 꺼내서 배열도 뒤에서 부터 넣음
+            answer[i] = answerStack.pop();
+        }
+
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         같은숫자는싫어 a = new 같은숫자는싫어();
 
@@ -36,6 +59,14 @@ public class 같은숫자는싫어 {
 
         for(int i = 0; i<result.length; i++){
             System.out.print(result[i]+" ");
+        }
+
+        System.out.println();
+
+        int[] result2 = a.solution2(arr);
+
+        for(int i = 0; i<result2.length; i++){
+            System.out.print(result2[i]+" ");
         }
     }
 }
