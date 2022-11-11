8104a33f1fdd5a8c4f7d84566ed86684362c42ae
yjyj1023 <lyj19961023@gmail.com>
2022-11-11 10:01:38 +0900

    비밀지도 - 이진수로 바꿔서 배열에 저장

--- src/비밀지도.java
+++ src/비밀지도.java
@@ -4,10 +4,28 @@ public class 비밀지도 {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = {};
 
+        int[][] arr1ToBinary = new int[n][n];
+        int[][] arr2ToBinary = new int[n][n];
+
+
         // arr1, arr2를 이진수로 바꿔서 저장
         for (int i = 0; i < arr1.length; i++) {
+            for (int j = n-1; j >= 0 ; j--) {
+                arr1ToBinary[i][j] = arr1[i]%2;
 
+                arr1[i] = arr1[i]/2;
+            }
         }
+
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = n-1; j >= 0 ; j--) {
+                arr2ToBinary[i][j] = arr2[i]%2;
+
+                arr2[i] = arr2[i]/2;
+            }
+        }
+
+        System.out.println(arr1ToBinary[1]);
 
         // 바꾼값들을 비교해서 같으면 # 다르면 공백 을 문자열에 저장
 
@@ -30,5 +48,9 @@ public class 비밀지도 {
         for (int i = 0; i < arr2.length; i++) {
             arr2[i] = sc.nextInt();
         }
+
+        비밀지도 a = new 비밀지도();
+
+        a.solution(n, arr1, arr2);
     }
 }
