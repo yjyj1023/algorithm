eb3dfb7101d4387cf1d69d3cb91f1cf423f1d777
yjyj1023 <lyj19961023@gmail.com>
2022-11-11 10:34:33 +0900

    비밀지도 - 비트연산추가

--- src/비밀지도.java
+++ src/비밀지도.java
@@ -1,12 +1,12 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class 비밀지도 {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
+    public String[] solution(int n, int[] arr1, int[] arr2) {  //배열로 풀기
         String[] answer = {};
 
         int[][] arr1ToBinary = new int[n][n];
         int[][] arr2ToBinary = new int[n][n];
-
 
         // arr1, arr2를 이진수로 바꿔서 저장
         for (int i = 0; i < arr1.length; i++) {
@@ -29,16 +29,42 @@ public class 비밀지도 {
 
         // 바꾼값들을 비교해서 둘중 하나만 1이면 # 둘다 0이면 공백을 문자열에 저장
         for (int i = 0; i < arr1ToBinary.length; i++) {
-            answer[i]="";
+            answer[i]="";  //초기화
             for (int j = 0; j < arr1ToBinary[i].length; j++) {
-                if(arr1ToBinary[i][j] == 1| arr2ToBinary[i][j]==1){
+                if(arr1ToBinary[i][j]==1 | arr2ToBinary[i][j]==1){
                     answer[i]+="#";
                 }else{
                     answer[i]+=" ";
                 }
             }
         }
+        return answer;
+    }
 
+    String getBinaryString(int decNum) {
+        String answer = "";
+        while (decNum > 0) {
+            answer += decNum % 2;
+            decNum /= 2;
+        }
+        return answer;
+    }
+
+    String solution2(int n, int[] arr1, int[] arr2) {  //비트연산으로 풀기
+        String answer = "";
+        String[] sArr1 = new String[n];
+        String[] sArr2 = new String[n];
+        for (int i = 0; i < n; i++) {
+            // arr1
+            for (int j = 0; j < n; j++) {
+                String binStr1 = getBinaryString(arr1[i]);
+                String binStr2 = getBinaryString(arr2[i]);
+                sArr1[i] = "0".repeat(n - binStr1.length()) + binStr1;
+                sArr2[i] = "0".repeat(n - binStr2.length()) + getBinaryString(arr2[i]);
+            }
+        }
+        System.out.println(Arrays.toString(sArr1));
+        System.out.println(Arrays.toString(sArr2));
 
         return answer;
     }
