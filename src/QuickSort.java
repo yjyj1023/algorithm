21ad538e23066b227d376ba9f0581cb32d57fbb5
yjyj1023 <lyj19961023@gmail.com>
2022-11-17 10:19:03 +0900

    재귀 추가

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -6,20 +6,25 @@ public class QuickSort {
 
         return arr;
     }
-    public int[] quickSort(int[] arr){
-        int pivot = arr[arr.length/2];
-        int leftIdx = 0;
-        int rightIdx = arr.length-1;
+    public int[] quickSort(int[] arr, int startIdx, int endIdx){
+
+        int pivot = arr[(startIdx+endIdx)/2];
+        int leftIdx = startIdx;
+        int rightIdx = endIdx;
 
         while(leftIdx<=rightIdx){
-            while(arr[leftIdx]<pivot) leftIdx ++;
+            while(arr[leftIdx]<pivot) leftIdx += 1;
             while(arr[rightIdx]>pivot) rightIdx -= 1;
 
-            arr = Swap(arr,leftIdx,rightIdx);
-            leftIdx+=1;
-            rightIdx-=1;
-
+            if(leftIdx <= rightIdx){
+                arr = Swap(arr,leftIdx,rightIdx);
+                leftIdx+=1;
+                rightIdx-=1;
+            }
         }
+
+        if(startIdx<rightIdx)quickSort(arr, startIdx, rightIdx);
+        if(leftIdx<endIdx)quickSort(arr, leftIdx, endIdx);
 
         return arr;
     }
@@ -27,7 +32,7 @@ public class QuickSort {
         int[] arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
         QuickSort qs = new QuickSort();
 
-        int[] result = qs.quickSort(arr);
+        int[] result = qs.quickSort(arr, 0, arr.length-1);
 
         for(int i: result){
             System.out.println(i);
