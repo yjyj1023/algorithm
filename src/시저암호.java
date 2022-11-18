34649f2a120a5d7d8080c7445127b480cf2056a4
yjyj1023 <lyj19961023@gmail.com>
2022-11-18 10:11:19 +0900

    프로그래머스-시저암호 완성

--- src/시저암호.java
+++ src/시저암호.java
@@ -5,13 +5,13 @@ public class 시저암호 {
             if(s.charAt(i) >= 'A' & s.charAt(i) <= 'Z'){  //대문자일 경우
                 if (!((s.charAt(i)+n)>='A' & (s.charAt(i)+n)<='Z')) {
                     answer = answer + String.valueOf((char)(s.charAt(i)+n-26));
-                }else if(s.charAt(i) != ' '){
+                }else{
                     answer = answer + String.valueOf((char)(s.charAt(i)+n));
                 }
             }else if (s.charAt(i) >= 'a' & s.charAt(i) <= 'z'){  //소문자일 경우
                 if (!((s.charAt(i)+n)>='a' & (s.charAt(i)+n)<='z')) {
                     answer = answer + String.valueOf((char)(s.charAt(i)+n-26));
-                }else if(s.charAt(i) != ' '){
+                }else{
                     answer = answer + String.valueOf((char)(s.charAt(i)+n));
                 }
             }else{  //공백일 경우
