92b3b3205992c11b4e8ef02df0d2d4a819c95641
yjyj1023 <lyj19961023@gmail.com>
2022-11-25 09:49:28 +0900

    최대공약수 재귀x

--- src/최대공약수.java
+++ src/최대공약수.java
@@ -1,5 +1,5 @@
 public class 최대공약수 {
-    public int gcd(int a, int b){
+    public int gcd(int a, int b){  //재귀 o
         if(a==b) {
             return a;
         }else if (a>b){
@@ -10,9 +10,22 @@ public class 최대공약수 {
         return 1;
     }
 
+    public int gcd2(int a, int b){  //재귀 x
+        while(a!=b){
+            if(a>b){
+                a = a-b;
+            }else{
+                b = b-a;
+            }
+        }
+        return a;
+    }
+
+
+
     public static void main(String[] args) {
         최대공약수 a = new 최대공약수();
 
-        System.out.println(a.gcd(196, 42));
+        System.out.println(a.gcd2(196, 42));
     }
 }
