3fe22806a3a625ece1914fa1f964818565e07ef4
yjyj1023 <lyj19961023@gmail.com>
2022-11-29 10:20:35 +0900

    자릿수 구하기

--- src/기수정렬.java
+++ src/기수정렬.java
@@ -1,4 +1,24 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class 기수정렬 {
+    public int[] getDigits(int[] arr){
+        Set<Integer> digits = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == 0) digits.add(1);
+            else digits.add((int)(Math.log10(arr[i])+1));
+        }
+
+        // set -> arr
+        int[] answer = new int[digits.size()];
+        int idx = 0;
+        for (Integer num : digits) {
+            answer[idx++] = num;
+        }
+        Arrays.sort(answer); //정렬
+        return answer;
+    }
     public int[] sort(int[] arr){
         int[] sortedArr = new int[10];
 
@@ -17,7 +37,7 @@ public class 기수정렬 {
         int[] arr = {7,4,5,9,1,0};
 
         기수정렬 s = new 기수정렬();
-        int[] result = s.sort(arr);
+        int[] result = s.getDigits(arr);
 
         for (int i: result){
             if(i != -1){
