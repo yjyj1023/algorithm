5f1cf1dfb73d55726176e82bf12b8f4bfdf77376
yjyj1023 <lyj19961023@gmail.com>
2022-12-01 09:41:11 +0900

    우선순위큐 사용해서 해결 중복된것 제거, 주석추가

--- src/명예의전당.java
+++ src/명예의전당.java
@@ -5,23 +5,20 @@ public class 명예의전당 {
         int[] answer = new int[score.length];
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < score.length; i++) {
-            if (pq.size() < k){
-                pq.add(score[i]);
-                answer[i] = pq.peek();
 
-            }else {
-                if (pq.peek() < score[i]){
+        for (int i = 0; i < score.length; i++) {
+            if (pq.size() < k) {   //pq의 크기가 k보다 작을 때
+                pq.add(score[i]);
+            }
+            else {   //pq의 크기가 k와 같을 때
+                if (pq.peek() < score[i]) {  //pq의 제일 작은 값이 새로들어온 score 보다 작을 때
                     pq.poll();
                     pq.add(score[i]);
-                    answer[i] = pq.peek();
-                }else {
-                    answer[i] = pq.peek();
                 }
-
             }
-
+            answer[i] = pq.peek();  //answer 에 제일 작은 값을 넣음
         }
+
         return answer;
     }
 
@@ -30,8 +27,8 @@ public class 명예의전당 {
         int[] arr = {10, 100, 20, 150, 1, 100, 200};
         int[] result = q.solution(3, arr);
 
-        for(int i: result){
-            System.out.print(i+" ");
+        for (int i : result) {
+            System.out.print(i + " ");
         }
 
         System.out.println();
@@ -39,8 +36,8 @@ public class 명예의전당 {
         int[] arr2 = {0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
         int[] result2 = q.solution(4, arr2);
 
-        for(int i: result2){
-            System.out.print(i+" ");
+        for (int i : result2) {
+            System.out.print(i + " ");
         }
     }
 }
