d5a8c6d174bdc095199a2966b0b442bdd4a4262c
yjyj1023 <lyj19961023@gmail.com>
2022-12-12 09:28:13 +0900

    3x3 배열만큼 dp 선언하기

---
+++ src/MinCostPath.java
@@ -0,0 +1,13 @@
+public class MinCostPath {
+    public int minCost(int[][] arr){
+        int[][] dp = new int[arr.length][arr[0].length];
+
+        return 1;
+    }
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,3,2},
+                {4,6,2},
+                {1,2,4}};
+    }
+}
